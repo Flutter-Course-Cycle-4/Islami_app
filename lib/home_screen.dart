0f01e4a262c7b307e19d08e37b05f90855cc822a
Muhammed Aly <40716171+muhammed3ly@users.noreply.github.com>
2021-10-22 13:53:58 +0200

    added home screen with common components

---
+++ lib/home_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late int currentScreenIndex;
+  @override
+  void initState() {
+    super.initState();
+    currentScreenIndex = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage('assets/images/bg3.png'),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Text(
+            'إسلامي',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentScreenIndex,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Color.fromRGBO(183, 147, 95, 1),
+          showUnselectedLabels: false,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Colors.black,
+          iconSize: 36,
+          selectedLabelStyle: TextStyle(
+            fontSize: 16,
+          ),
+          onTap: (index) {
+            setState(() {
+              currentScreenIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage('assets/icons/radio_ic.png'),
+                ),
+                label: 'راديو'),
+            BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage('assets/icons/sebha_ic.png'),
+                ),
+                label: 'تسبيح'),
+            BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage('assets/icons/hadeth_ic.png'),
+                ),
+                label: 'الحديث'),
+            BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage('assets/icons/quran_ic.png'),
+                ),
+                label: 'القرآن'),
+          ],
+        ),
+      ),
+    );
+  }
+}
